a70abff71577b88e8961ef45b1aed3b774dafe86
Brianna Lee <bornbrie@icloud.com>
2018-06-20 00:59:31 -0700

    Initial Commit

---
+++ FontBook/FontBook.h
@@ -0,0 +1,19 @@
+//
+//  FontBook.h
+//  FontBook
+//
+//  Created by Brie on 6/20/18.
+//  Copyright © 2018 Brianna Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontBook.
+FOUNDATION_EXPORT double FontBookVersionNumber;
+
+//! Project version string for FontBook.
+FOUNDATION_EXPORT const unsigned char FontBookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontBook/PublicHeader.h>
+
+
